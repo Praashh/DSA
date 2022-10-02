c2d43ec47ec2d20e69bb1406e16a709fa41464c4
Prashant varma <99237795+9520prashant@users.noreply.github.com>
2022-10-02 15:54:35 +0530

    LEFT VIEW

---
+++ Left_View.cpp
@@ -0,0 +1,27 @@
+// Left View of Binary Tree
+
+// T.C->O(N) and S.C->O(N)
+
+void leftView(Node*root,vector<int>&ans,int lvl){
+     // base case
+     if(root==NULL){
+         return ;
+     }
+     if(lvl==ans.size()){
+         ans.push_back(root->data);
+         
+     }
+    
+    leftView(root->left,ans,lvl+1);
+   
+    leftView(root->right,ans,lvl+1);
+     
+}
+//Function to return a list containing elements of left view of the binary tree.
+vector<int> leftView(Node *root)
+{
+   int lvl=0;
+   vector<int>ans;
+   leftView(root,ans,lvl);
+   return ans;
+}
