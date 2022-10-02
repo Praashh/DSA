1b8f820133730afb3565f7a898b404f5d822f4af
Prashant varma <99237795+9520prashant@users.noreply.github.com>
2022-10-02 15:54:52 +0530

    RIGHT

---
+++ Right_View.cpp
@@ -0,0 +1,32 @@
+// Right View of Binary Tree 
+
+// T.C-> O(N) and S.C->O(N)
+
+void leftView(Node*root,vector<int>&ans,int lvl){
+     // base case
+     if(root==NULL){
+         return ;
+     }
+     if(lvl==ans.size()){
+         ans.push_back(root->data);
+         
+     }
+     leftView(root->right,ans,lvl+1);
+     leftView(root->left,ans,lvl+1);
+   
+   
+    
+     
+}
+class Solution
+{
+    public:
+    //Function to return list containing elements of right view of binary tree.
+    vector<int> rightView(Node *root)
+    {
+         int lvl=0;
+         vector<int>ans;
+       leftView(root,ans,lvl);
+   return ans;
+    }
+};
